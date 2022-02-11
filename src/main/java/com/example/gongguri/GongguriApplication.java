e1e13377dee4a003e4c2faf94ab032c41b827b72
hgu5141 <hgu5141@naver.com>
2022-02-11 17:08:22 +0900

    세팅

---
+++ src/main/java/com/example/gongguri/GongguriApplication.java
@@ -0,0 +1,13 @@
+package com.example.gongguri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongguriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GongguriApplication.class, args);
+    }
+
+}
